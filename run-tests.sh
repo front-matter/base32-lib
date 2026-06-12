bf1b3b188660eecb127031f6d5db1c877b6ab127
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-12 18:47:24 +0600

    chore(tests): align run-tests.sh with other packages

--- run-tests.sh
+++ run-tests.sh
@@ -1,10 +1,37 @@
-#!/bin/sh
-# SPDX-FileCopyrightText: 2019 CERN.
-# SPDX-FileCopyrightText: 2019 Northwestern University.
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2019-2021 CERN.
+# SPDX-FileCopyrightText: 2019-2020 Northwestern University.
+# SPDX-FileCopyrightText: 2021-2026 TU Wien.
+# SPDX-FileCopyrightText: 2022 Graz University of Technology.
 # SPDX-License-Identifier: MIT
+#
+# Usage:
+#   ./run-tests.sh [pytest options and args...]
 
-pydocstyle base32-lib tests && \
-isort --recursive --check-only --diff **/*.py && \
-check-manifest --ignore ".travis-*" && \
-sphinx-build -qnNW docs docs/_build/html && \
-python setup.py test
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+# Check for arguments
+# Note: "-k" would clash with "pytest"
+pytest_args=()
+for arg in $@; do
+	# from the CLI args, filter out some known values and forward the rest to "pytest"
+	# note: we don't use "getopts" here b/c of some limitations (e.g. long options),
+	#       which means that we can't combine short options (e.g. "./run-tests -Kk pattern")
+	case ${arg} in
+		*)
+			pytest_args+=( ${arg} )
+			;;
+	esac
+done
+
+python -m check_manifest
+python -m sphinx.cmd.build -qnN docs docs/_build/html
+# Note: expansion of pytest_args looks like below to not cause an unbound
+# variable error when 1) "nounset" and 2) the array is empty.
+python -m pytest ${pytest_args[@]+"${pytest_args[@]}"}
+tests_exit_code=$?
+exit "$tests_exit_code"
