15922e51d3b0a1dba78c7a15b1fa7f1467c902c6
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-12 18:26:45 +0600

    chore(format): reformat with black

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,6 @@ from __future__ import print_function
 
 import os
 
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
